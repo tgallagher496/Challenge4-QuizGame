f0c60226586e8743398361e5bdcdd3fb41537012
tgallagher496 <tgallagher496@gmail.com>
2022-05-29 20:47:53 -0400

    restart

--- game.js
+++ game.js
@@ -1,72 +1,88 @@
-const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
-console.log(choices);
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
 
-let currentQuestion = {};
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
+let shuffledQuestions, currentQusetionIndex;
 
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () =>{
+    currentQuestionIndex++
+    setNextQuestion();
+})
 
-
-
-let questions = [
-{
-    question: "What is 2x2?",
-    choice1: "4",
-    choice2: "3",
-    choice3:  "1",
-    choice4: "0",
-    answer: 1
-},
-{
-    question: "What is 4x4",
-    choice1: "4",
-    choice2: "3",
-    choice3:  "1",
-    choice4: "16",
-    answer: 4
-},
-
-{
-    question: "What is 0x3?",
-    choice1: "4",
-    choice2: "0",
-    choice3:  "1",
-    choice4: "3",
-    answer: 4
+function startGame() {
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
 }
 
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  })
+  if(shuffledQuestions.length > currentQuestionIndex +1){
+    nextButton.classList.remove('hide')
+  } else{
+      startButton.innerText = "Restart"
+      startButton.classList.remove('hide')
+  }
+
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+const questions = [
+  {
+    question: "What is 2+2",
+    answers: [
+      { text: "4", correct: true },
+      { text: "22", correct: false },
+    ],
+  },
 ];
-
-const correct_bonus = 10;
-const max_questions = 3;
-
-function startGame()
-{
-    questioncounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    console.log(availableQuestions);
-    getNewQuestion();
-
-};
-
-function getNewQuestion()
-{
-questionCounter++;
-const questionIndex = Math.floor(Math.random()*availableQuestions.length);
-currentQuestion = availableQuestions[questionIndex];
-question.innerHTML = currentQuestion.question;
-
-choices.forEach(choice)
-{
-const number = choice.dataset['number'];
-choice.innerHTML = currentQuestion['choice'+number];
-};
-
-
-}
-
-startGame();
